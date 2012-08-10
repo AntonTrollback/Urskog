27d5a2b9a832e0925e1193680588f8b3f5e75a87
Anton Trollbäck <anton@trollback.se>
2012-08-10 17:17:41 +0200

    Add all first level subpages

--- app.rb
+++ app.rb
@@ -1,7 +1,11 @@
 require 'sinatra/base'
 
 class MyApp < Sinatra::Base
-  @home = false
+  @home      = false
+  @about     = true
+  @products  = true
+  @retailers = true
+  @contact   = true
 
   get '/' do
     @home = true
@@ -9,8 +13,27 @@ class MyApp < Sinatra::Base
   end
 
   get '/about/?' do
-     @title = "About -"
-     erb :about
+    @about = true
+    @title = "About -"
+    erb :about
+  end
+
+  get '/products/?' do
+    @products = true
+    @title = "Products -"
+    erb :products
+  end
+
+  get '/retailers/?' do
+    @retailers = true
+    @title = "Retailers -"
+    erb :retailers
+  end
+
+  get '/contact/?' do
+    @contact = true
+    @title = "Contact -"
+    erb :contact
   end
 
   run! if app_file == $0
