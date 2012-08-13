1804cdd0b039a2ff8a9b902c8fc285cb1d54b4c1
Anton Trollbäck <anton@trollback.se>
2012-08-13 16:08:53 +0200

    Add product subpages

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@ require 'sinatra/base'
 
 class MyApp < Sinatra::Base
   @active_page = "home"
+  @active_subpage = false
 
   get '/' do
     erb :index
@@ -30,6 +31,60 @@ class MyApp < Sinatra::Base
     @title = "Contact -"
     erb :contact
   end
+
+
+  # Subpages
+
+  get '/products/stubbe?' do
+    @active_page = "products"
+    @active_subpage = "stubbe"
+    @title = "Stubbe -"
+    erb :stubbe
+  end
+
+  get '/products/kvist?' do
+    @active_page = "products"
+    @active_subpage = "kvist"
+    @title = "Kvist -"
+    erb :kvist
+  end
+
+  get '/products/kil?' do
+    @active_page = "products"
+    @active_subpage = "kil"
+    @title = "Kil -"
+    erb :kil
+  end
+
+  get '/products/lov?' do
+    @active_page = "products"
+    @active_subpage = "lov"
+    @title = "L%C3%B6v -"
+    erb :lov
+  end
+
+  get '/products/pinne?' do
+    @active_page = "products"
+    @active_subpage = "pinne"
+    @title = "Pinne -"
+    erb :pinne
+  end
+
+  get '/products/sticka?' do
+    @active_page = "products"
+    @active_subpage = "sticka"
+    @title = "Sticka -"
+    erb :sticka
+  end
+
+  get '/products/fro?' do
+    @active_page = "products"
+    @active_subpage = "fro"
+    @title = "Fr%C3%B6 -"
+    erb :fro
+  end
+
+
 
   run! if app_file == $0
 
