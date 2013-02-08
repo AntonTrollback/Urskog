821cfa7225200fec5fd70db99d33b6750763bc18
Anton Trollbäck <anton@trollback.se>
2013-02-08 15:39:38 +0100

    Better handling of <title>s

--- app.rb
+++ app.rb
@@ -1,90 +1,94 @@
+# encoding: utf-8
+
 require 'sinatra/base'
 
 class MyApp < Sinatra::Base
-  @active_page = "home"
-  @active_subpage = false
+
+  def title
+    if defined? @title
+      "#{@title} — Urskog Longboard"
+    else
+      "Urskog Longboard"
+    end
+  end
+
+  def active(item)
+    "is-active" if item == @slug
+  end
 
   get '/' do
+    @slug = "home"
     erb :index
   end
 
   get '/about/?' do
-    @active_page = "about"
-    @title = "About -"
+    @slug = "about"
+    @title = "About"
     erb :about
   end
 
   get '/products/?' do
-    @active_page = "products"
-    @title = "Products -"
+    @slug = "products"
+    @title = "Products"
     erb :products
   end
 
   get '/retailers/?' do
-    @active_page = "retailers"
-    @title = "Retailers -"
+    @slug = "retailers"
+    @title = "Retailers"
     erb :retailers
   end
 
   get '/contact/?' do
-    @active_page = "contact"
-    @title = "Contact -"
+    @slug = "contact"
+    @title = "Contact"
     erb :contact
   end
 
 
-  # Subpages
+  # Products
 
   get '/products/stubbe?' do
-    @active_page = "products"
-    @active_subpage = "stubbe"
-    @title = "Stubbe -"
+    @slug = "products"
+    @title = "Stubbe"
     erb :stubbe
   end
 
   get '/products/kvist?' do
-    @active_page = "products"
-    @active_subpage = "kvist"
-    @title = "Kvist -"
+    @slug = "products"
+    @title = "Kvist"
     erb :kvist
   end
 
   get '/products/kil?' do
-    @active_page = "products"
-    @active_subpage = "kil"
-    @title = "Kil -"
+    @slug = "products"
+    @title = "Kil"
     erb :kil
   end
 
   get '/products/lov?' do
-    @active_page = "products"
-    @active_subpage = "lov"
-    @title = "L%C3%B6v -"
+    @slug = "products"
+    @title = "L%C3%B6v"
     erb :lov
   end
 
   get '/products/pinne?' do
-    @active_page = "products"
-    @active_subpage = "pinne"
-    @title = "Pinne -"
+    @slug = "products"
+    @title = "Pinne"
     erb :pinne
   end
 
   get '/products/sticka?' do
-    @active_page = "products"
-    @active_subpage = "sticka"
-    @title = "Sticka -"
+    @slug = "products"
+    @title = "Sticka"
     erb :sticka
   end
 
   get '/products/fro?' do
-    @active_page = "products"
-    @active_subpage = "fro"
-    @title = "Fr%C3%B6 -"
-    erb :fro
+    @slug = "products"
+    @title = "Frö"
+    erb :@slug
   end
-
-
 
   run! if app_file == $0
 
