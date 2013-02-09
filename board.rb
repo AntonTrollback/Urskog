c3eb265903d15367ffe76be86de0aa801e740eb1
Anton Trollbäck <anton@trollback.se>
2013-02-09 20:33:12 +0100

    Move board class to new file

---
+++ board.rb
@@ -0,0 +1,46 @@
+# encoding: utf-8
+
+require 'yaml'
+
+class Board
+
+  def self.all
+    @yaml_file ||= YAML.load_file("boards.yml")
+    @boards    ||= boards(@yaml_file)
+  end
+
+  def self.find(slug)
+    boards = self.all
+    boards.select {|b| b.slug == slug }.first || NullBoard.new
+  end
+
+  def self.boards(yaml_file)
+    boards = []
+    yaml_file.each do |item|
+      boards << Board.new(item)
+    end
+    boards
+  end
+
+  attr_accessor :name,
+                :length,
+                :price,
+                :slug
+
+  def initialize(attributes={})
+    attributes.each do |name, value|
+      self.public_send("#{name}=", value)
+    end
+  end
+
+end
+
+class NullBoard
+  def name
+    ""
+  end
+
+  def slug
+    ""
+  end
+end
