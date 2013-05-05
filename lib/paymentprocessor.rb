2b1a375893e469ecaf0efab6fefe53607eb3bf9b
Christopher Schmölzer <christopher.schmolzer@gmail.com>
2013-05-05 15:30:48 +0200

    create a transaction with paymill

--- lib/paymentprocessor.rb
+++ lib/paymentprocessor.rb
@@ -1,5 +1,12 @@
+require 'paymill'
+require_relative 'paymill_config'
+
 class Paymentprocessor
-  def self.purchase(order)
-    true
+  def self.purchase(order, board, calculator)
+    Paymill::Transaction.create({
+      token:        order.token,
+      amount:       calculator.amount,
+      currency:     'SEK',
+    })
   end
 end
