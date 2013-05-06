a992cc84ff90e3897eafb6fedc7eb77bb5afb6ae
Christopher Schmölzer <christopher.schmolzer@gmail.com>
2013-05-06 19:49:57 +0200

    databasefile

--- app.rb
+++ app.rb
@@ -23,23 +23,7 @@ class MyApp < Sinatra::Base
 
   register Sinatra::ActiveRecordExtension
 
-  configure :development do
-    set :database_file, "config/database.yml"
-  end
-  configure :production do
-    db = URI.parse(ENV['HEROKU_POSTGRESQL_JADE_URL'])
-
-    ActiveRecord::Base.establish_connection(
-      :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-      :host     => db.host,
-      :port     => db.port,
-      :username => db.user,
-      :password => db.password,
-      :database => db.path[1..-1],
-      :encoding => 'utf8'
-    )
-  end
-
+  set :database_file, "config/database.yml"
 
   # Serve assets using this protocol
   set :assets_protocol, :https
