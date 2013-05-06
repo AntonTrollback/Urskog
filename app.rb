366d12eb93d77170209cfd4ff95c93993c61aa35
Christopher Schmölzer <christopher.schmolzer@gmail.com>
2013-05-06 19:41:59 +0200

    kukdatabas

--- app.rb
+++ app.rb
@@ -27,7 +27,17 @@ class MyApp < Sinatra::Base
     set :database_file, "config/database.yml"
   end
   configure :production do
-    set :database, ENV['DATABASE_URL']
+    db = URI.parse(ENV['DATABASE_URL'])
+
+    ActiveRecord::Base.establish_connection(
+      :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+      :host     => db.host,
+      :port     => db.port,
+      :username => db.user,
+      :password => db.password,
+      :database => db.path[1..-1],
+      :encoding => 'utf8'
+    )
   end
 
 
