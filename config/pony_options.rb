798edc1f2c2eca53c366c95a8174b405adcfa738
Christopher Schmölzer <christopher.schmolzer@gmail.com>
2013-05-19 21:01:00 +0200

    refactor pony options to a separate file

---
+++ config/pony_options.rb
@@ -0,0 +1,15 @@
+require 'pony'
+
+Pony.options = { 
+  :from => 'order@urskog.com', 
+  :via => :smtp, 
+  :via_options => { 
+    :address              => 'send.one.com',
+    :port                 => '2525',
+    :enable_starttls_auto => true,
+    :user_name            => 'order@urskog.com',
+    :password             => 'Openmail',
+    :authentication       => :plain, # :plain, :login, :cram_md5, no auth by default
+    :domain               => "urskog.com" # the HELO domain provided by the client to the server
+  } 
+}
--- lib/order_email.rb
+++ lib/order_email.rb
@@ -1,18 +1,4 @@
-require 'pony'
-
-Pony.options = { 
-  :from => 'order@urskog.com', 
-  :via => :smtp, 
-  :via_options => { 
-    :address              => 'send.one.com',
-    :port                 => '2525',
-    :enable_starttls_auto => true,
-    :user_name            => 'order@urskog.com',
-    :password             => 'Openmail',
-    :authentication       => :plain, # :plain, :login, :cram_md5, no auth by default
-    :domain               => "urskog.com" # the HELO domain provided by the client to the server
-  } 
-}
+require_relative '../config/pony_options'
 
 class OrderEmail
 
