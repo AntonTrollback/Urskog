49fe6bf96af3f9dc2c9d27d39461e91398bf82bb
Anton Trollbäck <anton@trollback.se>
2013-05-19 22:18:13 +0200

    Content for receipt and order mail

--- lib/order_email.rb
+++ lib/order_email.rb
@@ -2,10 +2,8 @@ require_relative '../config/pony_options'
 
 class OrderEmail
 
-  # TJOFFE MAIL
-
   attr_reader :order
-  
+
   def initialize(order)
     @order = order
   end
@@ -13,12 +11,37 @@ class OrderEmail
   def send
     Pony.mail({
       :to => 'christopher.schmolzer@gmail.com',
-      :subject => 'TJOFFES MAIL', 
+      :subject => 'Ping! Purchase: #{order.board} #{order.type_of_purchase}',
       :html_body => "
-        <p>Name: #{order.name}</p>
-        <p>Email: #{order.email}</p>
-        <p>Street: #{order.street}</p>
+Order information:
+Date: #{order.date}
+ID: #{order.id}
+Token: #{order.token}
+
+Product information:
+Model: #{order.board}
+Containing: #{order.type_of_purchase}
+Wood: #{order.wood_type}
+Amount: #{order.price_no_vat} sek (including VAT)
+
+Customer information:
+Name: #{order.name}
+Email address: #{order.email}
+
+Shipping information:
+Country: #{order.price_no_vat}
+City: #{order.price_no_vat}
+Street: #{order.price_no_vat}
+Postal code: #{order.price_no_vat}
+
+
+Have fun packaging!
+
+Dr. Website
       "
     })
   end
 end
+
+
+
--- lib/receipt_email.rb
+++ lib/receipt_email.rb
@@ -11,12 +11,58 @@ class ReceiptEmail
   def send
     Pony.mail({
       :to => order.email,
-      :subject => 'RECEIPT', 
+      :subject => 'Urskog Receipt',
       :html_body => "
-        <p>Name: #{order.name}</p>
-        <p>Email: #{order.email}</p>
-        <p>Street: #{order.street}</p>
+Hi #{order.name},
+
+Thanks for your purchase! This email serves as your receipt. If you have any questions, please contact us anytime at
+support@urskog.com.
+
+
+Email address: #{order.email}
+Date: #{order.date}
+Order ID: #{order.id}
+
+Retailer: Urskog AB
+VAT number: SE556797382001
+
+Product information:
+Model: #{order.board} #{order.type_of_purchase} in #{order.wood_type}
+Amount: #{order.price_no_vat} sek (including VAT)
+
+Shipping information:
+Country: #{order.price_no_vat}
+City: #{order.price_no_vat}
+Street: #{order.price_no_vat}
+Postal code: #{order.price_no_vat}
+
+
+We hope you enjoy your longboard!
+
+The Urskog team
       "
     })
   end
 end
+
+
+Dear AntonTrollback:
+
+This is a receipt for your GitHub subscription. This is only a receipt, no
+payment is due. If you have any questions, please contact us anytime at
+support@github.com. Thank you for your business!
+
+-------------------------------------------------
+GITHUB RECEIPT - 08 May 2013 - 11:44AM
+
+User: AntonTrollback
+Plan: Micro
+Amount: USD $7.00*
+
+
+GitHub, Inc.
+548 4th St.
+San Francisco, CA 94107
+
+* EU customers: prices exclude VAT
+-------------------------------------------------
