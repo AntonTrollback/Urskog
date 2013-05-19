64afafa78ccfb9e628ff9a948d58b89c6c1042cc
Christopher Schmölzer <christopher.schmolzer@gmail.com>
2013-05-19 22:33:22 +0200

    ANTONLOL

--- lib/receipt_email.rb
+++ lib/receipt_email.rb
@@ -44,25 +44,3 @@ The Urskog team
     })
   end
 end
-
-
-Dear AntonTrollback:
-
-This is a receipt for your GitHub subscription. This is only a receipt, no
-payment is due. If you have any questions, please contact us anytime at
-support@github.com. Thank you for your business!
-
--------------------------------------------------
-GITHUB RECEIPT - 08 May 2013 - 11:44AM
-
-User: AntonTrollback
-Plan: Micro
-Amount: USD $7.00*
-
-
-GitHub, Inc.
-548 4th St.
-San Francisco, CA 94107
-
-* EU customers: prices exclude VAT
--------------------------------------------------
