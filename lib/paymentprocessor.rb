eef1bb18b1f5a95c39759982acd4e36f09c69128
Christopher Schmölzer <christopher.schmolzer@gmail.com>
2013-07-30 16:54:51 +0200

    bug fix: return false unless response code is 20 000

--- lib/paymentprocessor.rb
+++ lib/paymentprocessor.rb
@@ -5,11 +5,16 @@ require_relative 'payment_error_email'
 class Paymentprocessor
   def self.purchase(order, board, calculator)
     begin 
-      Paymill::Transaction.create({
+      trans = Paymill::Transaction.create({
         token:        order.token,
         amount:       calculator.amount,
         currency:     'SEK',
       })
+      if trans.response_code == 20000
+        true
+      else
+        false
+      end
     rescue => error
       # Email admin
       order.payment_went_wrong = true
